da0d5e758d9ca79e115646f8b150f2c1464908f0
Traun Leyden <tleyden@couchbase.com>
2017-08-26 13:24:02 -0700

    ForEachDocInTargetBucket

--- main.go
+++ main.go
@@ -40,9 +40,9 @@ func (e *Example) Connect(connSpecStr string) (err error) {
 
 
 // A function that takes a doc id and returns an error
-type PostInsertCallback func(docId string) error
+type DocProcessor func(docId string) error
 
-func (e *Example) CopyBucket(postInsertCallback PostInsertCallback) (err error) {
+func (e *Example) CopyBucket(postInsertCallback DocProcessor) (err error) {
 
 	result := map[string]interface{}{}
 	_, err = e.SourceBucket.Get("airline_10", &result)
@@ -157,6 +157,57 @@ func (e *Example) GetSubdocField(docId, subdocKey string) (retValue interface{},
 
 }
 
+func (e *Example) SetSubdocField(docId, subdocKey string, subdocVal interface{}) (err error) {
+
+	_, err = e.TargetBucket.MutateInEx(docId, gocb.SubdocDocFlagNone, 0, 0).
+		UpsertEx(subdocKey, subdocVal, gocb.SubdocFlagNone).
+		Execute()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (e *Example) ForEachDocInTargetBucket(postInsertCallback DocProcessor) (err error) {
+	return e.ForEachDocInBucket(postInsertCallback, e.TargetBucket)
+}
+
+func (e *Example) ForEachDocInBucket(docProcessor DocProcessor, bucket *gocb.Bucket) (err error) {
+
+	// Get the doc ID and the doc body in a single query
+	query := gocb.NewN1qlQuery("SELECT META(`travel-sample`).id,* FROM `travel-sample`")
+	rows, err := e.SourceBucket.ExecuteN1qlQuery(query, nil)
+	if err != nil {
+		return err
+	}
+
+	row := map[string]interface{}{}
+	for rows.Next(&row) {
+
+		// Get row ID
+		rowIdRaw, ok := row["id"]
+		if !ok {
+			return fmt.Errorf("Row does not have id field")
+		}
+		rowIdStr, ok := rowIdRaw.(string)
+		if !ok {
+			return fmt.Errorf("Row id field not of expected type")
+		}
+
+		// Invoke the doc processor callback
+		if err := docProcessor(rowIdStr); err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+
 func main() {
 
 	// Create an example application and connect to a couchbase cluster
@@ -165,6 +216,7 @@ func main() {
 
 	// Create a post-insert callback function that will be invoked on
 	// every document that is copied from the source bucket and inserted into the target bucket.
+	// It adds the "DateCopied" XATTR to the doc.
 	xattrKey := "Metadata"
 	postInsertCallback := func(docId string) error {
 
@@ -200,7 +252,6 @@ func main() {
 	//	panic(fmt.Errorf("Error: %v", err))
 	//}
 
-
 	xattrVal, err := e.GetXattrs("airline_10123", xattrKey)
 	if err != nil {
 		panic(fmt.Errorf("Error: %v", err))
@@ -213,10 +264,38 @@ func main() {
 		panic(fmt.Errorf("Error: %v", err))
 	}
 
-	log.Printf("retVal: %+v", retValue)
+	log.Printf("airline_10123 type: %+v", retValue)
 
 
 	// Iterate over all docs and update the type field to app:<existing_type>
+
+	appendNamespaceToTypeField := func(docId string) error {
+
+		currentValueOfTypeField, err := e.GetSubdocField(docId, "type")
+		if err != nil {
+			return err
+		}
+
+		newValueOfTypeField := fmt.Sprintf("foo-component:%v", currentValueOfTypeField)
+
+		err = e.SetSubdocField(docId, "type", newValueOfTypeField)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	if err := e.ForEachDocInTargetBucket(appendNamespaceToTypeField); err != nil {
+		panic(fmt.Errorf("Error: %v", err))
+	}
+
+	retValue, err = e.GetSubdocField("airline_10123", "type")
+	if err != nil {
+		panic(fmt.Errorf("Error: %v", err))
+	}
+
+	log.Printf("airline_10123 type (after): %+v", retValue)
 
 
 }
